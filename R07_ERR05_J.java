79abac9c25fd19f96c30ef56d495ceefb3cf1ca2
gu637739 <gu637739@ucf.edu>
2025-03-02 18:16:05 -0500

    Create R07_ERR05_J.java

---
+++ R07_ERR05_J.java
@@ -0,0 +1,21 @@
+// Rule 07 - Exceptional Behavior (ERR)
+// ERR05-J: Do not let checked exceptions escape from a finally block
+// Non-compliant code
+
+public class Operation {
+  public static void doOperation(String some_file) {
+    // ... Code to check or set character encoding ...
+    try {
+      BufferedReader reader =
+          new BufferedReader(new FileReader(some_file));
+      try {
+        // Do operations
+      } finally {
+        reader.close();
+        // ... Other cleanup code ...
+      }
+    } catch (IOException x) {
+      // Forward to handler
+    }
+  }
+}
