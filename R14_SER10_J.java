4f61ce1bcfc9ca9d33ba5443686edeb7d7f8e3c1
gu637739 <gu637739@ucf.edu>
2025-03-02 18:25:29 -0500

    Update R14_SER10_J.java
    compliant solution

--- R14_SER10_J.java
+++ R14_SER10_J.java
@@ -26,3 +26,24 @@ class SerializeSensorData {
     }
   }
 }
+
+// Compliant solution:
+class SerializeSensorData {
+  public static void main(String[] args) throws IOException {
+    ObjectOutputStream out = null;
+    try {
+      out = new ObjectOutputStream(
+          new BufferedOutputStream(new FileOutputStream("ser.dat")));
+      while (SensorData.isAvailable()) {
+        // Note that each SensorData object is 1 MB in size
+        SensorData sd = SensorData.readSensorData();
+        out.writeObject(sd);
+        out.reset(); // Reset the stream
+      }
+    } finally {
+      if (out != null) {
+        out.close();
+      }
+    }
+  }
+}
