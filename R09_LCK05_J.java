f16e34c53aa54fbf6e56c18d24dad5baf5c0b06f
gu637739 <gu637739@ucf.edu>
2025-03-02 18:20:22 -0500

    Update R09_LCK05_J.java
    Compliant solution

--- R09_LCK05_J.java
+++ R09_LCK05_J.java
@@ -10,3 +10,16 @@ public final class CountHits {
   }
 }
 
+//Compliant Solution
+
+/* This class is thread-safe */
+public final class CountHits {
+  private static int counter;
+  private static final Object lock = new Object();
+ 
+  public void incrementCounter() {
+    synchronized (lock) {
+      counter++;
+    }
+  }
+}
