3090202e8f8ef3289a0488639a285c78c90574d7
gu637739 <gu637739@ucf.edu>
2025-03-02 18:17:35 -0500

    Update R07_ERR05_J.java
    Compliant solution

--- R07_ERR05_J.java
+++ R07_ERR05_J.java
@@ -19,3 +19,26 @@ public class Operation {
     }
   }
 }
+
+//Compliant Solution
+public class Operation {
+  public static void doOperation(String some_file) {
+    // ... Code to check or set character encoding ...
+    try {
+      BufferedReader reader =
+          new BufferedReader(new FileReader(some_file));
+      try {
+        // Do operations
+      } finally {
+        try {
+          reader.close();
+        } catch (IOException ie) {
+          // Forward to handler
+        }
+        // ... Other cleanup code ...
+      }
+    } catch (IOException x) {
+      // Forward to handler
+    }
+  }
+}
