fd3e012a8bca4532c3904ee7a2a9727a48128ec0
gu637739 <gu637739@ucf.edu>
2025-03-02 18:29:28 -0500

    Update R15_SEC02_J.java
    added compliant solution

--- R15_SEC02_J.java
+++ R15_SEC02_J.java
@@ -10,3 +10,15 @@ public RandomAccessFile openFile(final java.io.File f) {
     }
   });
 }
+
+//Compliant Solution 
+public RandomAccessFile openFile(java.io.File f) {
+  final java.io.File copy = new java.io.File(f.getPath());
+  askUserPermission(copy.getPath());
+  // ...
+  return (RandomAccessFile)AccessController.doPrivileged(new PrivilegedAction <Object>() {
+    public Object run() {
+      return new RandomAccessFile(copy, copy.getPath());
+    }
+  });
+}
