3b4f30bd084e68b7fd89b599a1e2a219799c79d5
gu637739 <gu637739@ucf.edu>
2025-03-02 17:58:54 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -8,3 +8,13 @@ public void deleteFile(){
   someFile.delete();
  
 }
+// Compliant Solution
+public void deleteFile(){
+ 
+  File someFile = new File("someFileName.txt");
+  // Do something with someFile
+  if (!someFile.delete()) {
+    // Handle failure to delete the file
+  }
+ 
+}
